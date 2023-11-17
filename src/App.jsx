0f14c75123253e21742035e5301a5c555f79bd99
unknowner1818 <aadhithya1837@gmail.com>
2023-11-17 08:54:33 +0530

    Added a core component and few dynamic rendering changes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,54 @@
+import imgPath from "./assets/react-core-concepts.png";
+const words = ["Fundamental", "Core", "Crucial"];
+import componentsImg from "./assets/components.png";
+function getDesc(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreComp(props) {
+    return (
+        <li>
+            <img src={props.img} alt="" />
+            <h2>{props.title}</h2>
+            <p>{props.desc}</p>
+        </li>
+    );
+}
+
+function Header() {
+    const description = words[getDesc(2)];
+    return (
+        <header>
+            <img src={imgPath} alt="Stylized atom" />
+            <h1>React Essentials</h1>
+            <p>
+                {description} React concepts you will need for almost any app
+                you are going to build!
+            </p>
+        </header>
+    );
+}
+
 function App() {
     return (
         <div>
-            <header>
-                <img
-                    src="src/assets/react-core-concepts.png"
-                    alt="Stylized atom"
-                />
-                <h1>React Essentials</h1>
-                <p>
-                    Fundamental React concepts you will need for almost any app
-                    you are going to build!
-                </p>
-            </header>
+            <Header />
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+                    <ul>
+                        <CoreComp
+                            title="Components"
+                            img={componentsImg}
+                            desc="This is the basic concept for learning react"
+                        ></CoreComp>
+                        <CoreComp
+                            title="Props"
+                            img={componentsImg}
+                            desc="This is the basic concept for learning react"
+                        ></CoreComp>
+                    </ul>
+                </section>
             </main>
         </div>
     );
